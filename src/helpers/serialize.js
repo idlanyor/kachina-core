39b0f9f2c2dd68e2f2070179a2b47ea891c81a4c
idlanyor <prasbhara0604@gmail.com>
2025-11-11 22:12:06 +0700

    fix(serialize): correct participant field in quoted message context
    Use msg.key.participant directly instead of conditional check for group chats to ensure consistent behavior

--- src/helpers/serialize.js
+++ src/helpers/serialize.js
@@ -69,7 +69,7 @@ export async function serialize(messages, sock) {
                 remoteJid: m.chat,
                 fromMe: msg.message[type].contextInfo.participant === sock.user.id,
                 id: msg.message[type].contextInfo.stanzaId,
-                participant: m.isGroup ? m.key.participantLid : m.key.participant
+                participant:  msg.key.participant
             },
             message: quoted,
             pushName: msg.message[type].contextInfo.pushName || ''
